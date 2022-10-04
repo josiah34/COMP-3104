71f1b99b31f93aa5f34f33e9db462da5fdfb1211
Josiah <josiah_g@live.com>
2022-10-03 20:18:16 -0400

    React Project Added

---
+++ build/precache-manifest.d87a02dd42849ae180bb3de2d2f101bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "11167a7cd5c87c80d772",
+    "url": "/josiah34/COMP-3104/static/css/main.fae40843.chunk.css"
+  },
+  {
+    "revision": "11167a7cd5c87c80d772",
+    "url": "/josiah34/COMP-3104/static/js/main.11167a7c.chunk.js"
+  },
+  {
+    "revision": "7a40e997d87c979b7e01",
+    "url": "/josiah34/COMP-3104/static/js/runtime~main.7a40e997.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/josiah34/COMP-3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "15162aebd18944ade07c043fa42fadc8",
+    "url": "/josiah34/COMP-3104/index.html"
+  }
+];
